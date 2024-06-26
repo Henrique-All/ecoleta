e3438d4814d3c6f33d004fef20eb47f8e770c409
CH <carlinhos.biocraft@gmail.com>
2024-06-25 21:07:46 -0300

    Feat: router create and configurator

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./views/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/Router';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
 );
